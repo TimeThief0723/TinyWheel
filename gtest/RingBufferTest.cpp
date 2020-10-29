5ec8ddedaf7bfa4820e83bc09d23a4643122a864
wangchen14 <wangchen14@bj-m-217662a.local>
2020-10-29 16:16:35 +0800

    RingBuffer

---
+++ gtest/RingBufferTest.cpp
@@ -0,0 +1,74 @@
+#include <gtest/gtest.h>
+#include <thread>
+#include <vector>
+#include <utility>
+#include <atomic>
+#include <deque>
+#include "RingBuffer.hpp"
+#include "DummyObject.h"
+
+using namespace std;
+using namespace TinyWheel;
+
+TEST(RingBuffer, grammar)
+{
+    RingBuffer<OjbectOnlyMove> b1(10);
+    OjbectOnlyMove a1,a2;
+    b1.push(move(a1));
+    b1.pop(a2);
+
+    RingBuffer<OjbectOnlyCopy> b2(10);
+    OjbectOnlyCopy b;
+    b2.push(b);
+    ASSERT_EQ(1,1);
+}
+
+TEST(RingBuffer, corner)
+{
+    RingBuffer<int> a(0);
+    ASSERT_EQ(true, a.full());
+    ASSERT_EQ(true, a.empty());
+
+    RingBuffer<int> b(1);
+
+    ASSERT_EQ(false, b.full());
+    ASSERT_EQ(true, b.empty());
+
+    b.push(12345);
+
+    ASSERT_EQ(true, b.full());
+    ASSERT_EQ(false, b.empty());
+    int t;
+    b.pop(t);
+
+    ASSERT_EQ(false, b.full());
+    ASSERT_EQ(true, b.empty());
+    ASSERT_EQ(12345, t);
+
+}
+
+TEST(RingBuffer, random)
+{
+    for(int i = 0; i < 10; i++){
+        int num = rand() % 10000;
+        RingBuffer<int> b(num);
+        deque<int> q;
+        for(int i = 0; i < num; i++){
+            b.push(i);
+            q.push_back(i);
+            if(i % 5 == 0){
+                int v;
+                b.pop(v);
+                ASSERT_EQ(v , q.front());
+                q.pop_front();
+            }
+        }
+        while(!b.empty() && !q.empty()){
+            int v;
+            b.pop(v);
+            ASSERT_EQ(v , q.front());
+            q.pop_front();
+        }
+        ASSERT_EQ(b.empty(), q.empty());
+    }
+}
---
+++ include/RingBuffer.hpp
@@ -0,0 +1,42 @@
+#pragma once
+#include <iostream>
+#include <utility>
+#include <vector>
+using namespace std;
+
+namespace TinyWheel{
+
+template<class T>
+class RingBuffer{
+public:
+    RingBuffer(size_t capacity):capacity_(capacity+1), data_(capacity_){ }
+
+    template<class TypeWithRef>
+    void push(TypeWithRef &&v){
+        data_[end_] = forward<TypeWithRef>(v);
+        end_ = (end_ + 1) % capacity_;
+    }
+
+    void pop(T &v){
+        v = move(data_[start_]);
+        start_ = (start_ + 1) % capacity_;
+    }
+
+    bool full(){
+        return start_ == ((end_ + 1) % capacity_);
+    }
+
+    bool empty(){
+        return start_ == end_;
+    }
+
+private:
+    size_t start_ = 0;
+    size_t end_ = 0;
+    size_t capacity_ = 0;
+    vector<T> data_;
+};
+
+
+
+};
